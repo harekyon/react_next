5f6c7e9fd712b3ed02fa7a93cce7d907bfa34853
harekyon <harekyon@gmail.com>
2022-10-13 00:00:14 +0900

    add file

---
+++ components/hero.js
@@ -0,0 +1,13 @@
+import styles from 'styles/hero.module.css'
+
+export default function Hero({title,subtitle,imageOn=false}){
+  return(
+    <dir className={styles.flexContainer}>
+      <div className={styles.text}>
+        <h1 className={styles.title}>{title}</h1>
+        <p className={styles.subtitle}>{subtitle}</p>
+        {imageOn && <figure> [画像] </figure>}
+      </div>
+    </dir>
+  )
+}
