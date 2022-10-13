2c47e2b15014b3b6a78dc79fd9f6299e876e4fce
k-hareyama <k-hareyama@H-COM-229-hareyama.local>
2022-10-13 16:50:36 +0900

    add images

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,7 @@
 import Container from 'components/container'
 import Hero from 'components/hero'
 import PostBody from 'components/post-body'
-import Contact from 'components/contact'
+// import Contact from 'components/contact'
 import {TwoColumn,TwoColumnMain,TwoColumnSidebar} from 'components/two-column'
 
 export default function About (){
